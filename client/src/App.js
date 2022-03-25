0802ef85a5c08e9be5a8b7dce70ca1181cda8645
Harindu Dilshan ( Kavinda Pitiduwa Gamage) <harindudilshan95@gmail.com>
2022-03-25 11:15:14 -0600

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Login.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Login">
+      <section className="login-section">
+        <h1> Quizzical </h1>
+        <label htmlFor="username">Username</label>
+        <input id="username"></input>
+        <button>Login</button>
+        <div className="padding"></div>
+      </section>
     </div>
   );
 }
